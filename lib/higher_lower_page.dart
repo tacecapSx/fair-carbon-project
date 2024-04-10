eb55be8ec6cd47c52a9a4ce426c3a9bc8e5c91ac
Oskar Holland <oskarwholland@gmail.com>
2024-04-10 13:24:45 +0200

    HigherLower page animation work in progress

--- lib/higher_lower_page.dart
+++ lib/higher_lower_page.dart
@@ -14,131 +14,192 @@ void sendDataToFirebase() {
   });
 }
 
-class HigherLowerPage extends StatelessWidget {
-    const HigherLowerPage({super.key});
+class HigherLowerPage extends StatefulWidget {
+  const HigherLowerPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-    //double windowHeight = MediaQuery.of(context).size.height;
-    //double windowWidth = MediaQuery.of(context).size.width;
+  @override
+  _HigherLowerPageState createState() => _HigherLowerPageState();
+}
+
+void animatePage(AnimationController _controller){
+  if (_controller.status == AnimationStatus.completed) {
+    _controller.reverse();
+  } else {
+    _controller.forward();
+  }
+}
+
+class _HigherLowerPageState extends State<HigherLowerPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  double windowWidth = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reset();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    windowWidth = MediaQuery.of(context).size.width;
+
+    _animation = Tween<Offset>(
+      begin: const Offset(0.0, 0.0),
+      end: Offset(windowWidth / 2, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
 
     return Scaffold(
-
-      //Display images
-      appBar: const HeaderWidget(),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  appBar: const HeaderWidget(),
+  body: AnimatedBuilder(
+    animation: _controller,
+    builder: (context, child) {
+      return Stack(
         children: [
-            Expanded(
-            child: AspectRatio(
-                aspectRatio: 0.01,
-                child: ClipRect(
-                    child: OverflowBox(
-                        alignment: Alignment.topLeft,
-                            child: Image.asset(
-                            'assets/forest_fire.jpg', // Path to the right image asset
-                            fit: BoxFit.cover,
-                        ),
-                    ),
-                ),
+          Positioned(
+            left: 0,
+            child: Image.asset(
+              'assets/private_jet.jpg',
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
             ),
-        ),
-          Expanded(
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return Stack(
-                  children: [
-                    OverflowBox(
-                      alignment: Alignment.topRight,
-                      child: Image.asset(
-                          'assets/apple_macbook_pro_16_2.jpg', // Path to the right image asset
+          ),
+          Transform.translate(
+            offset: _animation.value,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 0.01,
+                    child: ClipRect(
+                      child: OverflowBox(
+                        alignment: Alignment.topLeft,
+                        child: Image.asset(
+                          'assets/forest_fire.jpg',
                           fit: BoxFit.cover,
-                          height: constraints.maxHeight,
+                        ),
                       ),
                     ),
+                  ),
+                ),
 
-                    //Higher or lower buttons
-                    Center(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                            
-                        //Higher button
-                        SizedBox(
-                            width: 150,
-                            height: 60,
-                            child: TextButton(
-                                onPressed: () {
-                                    //   when button is pressed
-                                    sendDataToFirebase();
-                                },
-
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<Color>(
-                                        Colors.white.withOpacity(0.6),
-                                    ),
-
-                                    side: MaterialStateProperty.all(const BorderSide(
-                                    color: Colors.black,
-                                    width: 4.0, 
-                                    )),
-
-                                    foregroundColor: MaterialStateProperty.all<Color>(
-                                        Colors.green,
-                                    ),
-                                ),
-                                child: const Text(
-                                        'Higher ▲',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20,
-                                        ),
-                                    ),
-                                ),
+                Expanded(
+                  child: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                      return Stack(
+                        children: [
+                          OverflowBox(
+                            alignment: Alignment.topRight,
+                            child: Image.asset(
+                              'assets/apple_macbook_pro_16_2.jpg',
+                              fit: BoxFit.cover,
+                              height: constraints.maxHeight,
                             ),
-                        
-                        //Lower button
-                        const SizedBox(height: 20),
-                        SizedBox(
-                            width: 150,
-                            height: 60, 
-                            child: TextButton(
-                                onPressed: () {
-                                    //   when button is pressed
-                                },
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<Color>(
-                                        Colors.white.withOpacity(0.6), 
+                          ),
+                          Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 150,
+                                  height: 60,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      sendDataToFirebase();
+                                      if (_controller.status != AnimationStatus.forward &&
+                                          _controller.status != AnimationStatus.completed) {
+                                        _controller.forward();
+                                      } else {
+                                        _controller.reverse();
+                                      }
+                                    },
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(
+                                        Colors.white.withOpacity(0.6),
+                                      ),
+                                      side: MaterialStateProperty.all(
+                                        const BorderSide(
+                                          color: Colors.black,
+                                          width: 4.0,
+                                        ),
+                                      ),
+                                      foregroundColor: MaterialStateProperty.all<Color>(
+                                        Colors.green,
+                                      ),
                                     ),
-
-                                    side: MaterialStateProperty.all(const BorderSide(
-                                    color: Colors.black,
-                                    width: 4.0, 
-                                    )),
-
-                                    foregroundColor: MaterialStateProperty.all<Color>(
+                                    child: const Text(
+                                      'Higher ▲',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                SizedBox(
+                                  width: 170,
+                                  height: 60,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(
+                                        Colors.white.withOpacity(0.6),
+                                      ),
+                                      side: MaterialStateProperty.all(
+                                        const BorderSide(
+                                          color: Colors.black,
+                                          width: 4.0,
+                                        ),
+                                      ),
+                                      foregroundColor: MaterialStateProperty.all<Color>(
                                         Colors.red,
+                                      ),
                                     ),
-                                ),
-                                child: const Text(
-                                    'Lower ▼',
-                                    style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
+                                    child: const Text(
+                                      'Lower ▼',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
                                     ),
+                                  ),
                                 ),
+                              ],
                             ),
                           ),
                         ],
-                      ),
-                    ),
-                  ],
-                );
-              },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
-      ),
-    );
+      );
+    },
+  ),
+);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
